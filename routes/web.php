ce988d929d85fc3ca4d9fbcf35ac4d309917ecb9
MatteoLoddo <loddomatteo96@gmail.com>
2022-08-03 15:30:31 +0200

    add delete

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,11 @@ Route::get('/comics/create' , 'ComicController@create')->name('comics.create');
 // rotta per visualizzare un singolo fumetto
 Route::get('/comics/{comic}', 'ComicController@show')->name('comics.show');
 
-ROute::get('/comics/{comic}/edit' , 'ComicController@edit')->name("comics.edit");
+Route::get('/comics/{comic}/edit' , 'ComicController@edit')->name("comics.edit");
 Route::patch('/comics/{comic}' , 'ComicController@update')->name('comics.update');
 
 
-
+Route::delete('/comics/{comic}' , 'ComicController@destroy')->name('comics.destroy');
 
 
 // rotta per salvare i nuovi fumetti inseriti
